15680b5c85ae7d183aeac7b81163a7410deb7b24
Yoones Khoshghadam <yooneskh@gmail.com>
2022-10-25 17:53:58 +0330

    feat (jobs): add work queue

--- deps.ts
+++ deps.ts
@@ -4,3 +4,7 @@ export {
   UnifiedRouter,
   UnifiedWebServer,
 } from 'https://deno.land/x/unified_webserver@v0.2.0/mod.ts';
+
+export {
+  PatternEventEmitter,
+} from 'https://deno.land/x/unified_event_emitter@v0.1.0/mod.ts';
--- unified-jobs/mod.ts
+++ unified-jobs/mod.ts
@@ -1,10 +1,16 @@
+import { PatternEventEmitter } from '../deps.ts';
 
+
+type TWorkStages = 'fresh' | 'active' | 'waiting' | 'finished' | 'rejected';
 
 export interface IWork {
-  stage: 'initiated' | 'finished' | 'rejected' | string;
+  _id: string;
+  stage: TWorkStages;
+  type: string;
   context: Record<string, unknown>;
-  history: { // initiated and finished should be in this too
+  history: { // fresh and finished should be in this too
     stage: string;
+    type: string;
     context: Record<string, unknown>;
     createdAt: number;
   }[];
@@ -22,17 +28,84 @@ export interface IWorkerContext {
 }
 
 
-export async function registerWorker(stage: string, handler: (context: IWorkerContext) => Promise<void>) {
+const _eventEmitter = new PatternEventEmitter(false);
+
+const workers: { type: string, handler: (context: IWorkerContext) => Promise<{ stage: TWorkStages, type: string; context: Record<string, unknown>}>}[] = [];
+const workQueue: IWork[] = [];
+
+
+// deno-lint-ignore require-await
+export async function registerWorker(type: string, handler: (context: IWorkerContext) => Promise<{ stage: TWorkStages, type: string; context: Record<string, unknown>}>) {
+
+  workers.push({ type, handler });
+
+  // todo: create a DAG of workers !!!!!!!!!!!
 
 }
 
 
-export async function submitWork(stage: string, initialContext: Record<string, unknown>, onFinish: (work: IWork) => Promise<void>) {
+// deno-lint-ignore require-await no-unused-vars
+export async function submitWork(type: string, initialContext: Record<string, unknown>, onFinish: (work: IWork) => Promise<void>) {
 
-  // create work
+  const work: IWork = {
+    _id: String(Math.random()),
+    stage: 'fresh',
+    type,
+    context: initialContext,
+    history: [
+      {
+        stage: 'fresh',
+        type,
+        context: initialContext,
+        createdAt: Date.now(),
+      },
+    ],
+    createdAt: Date.now(),
+  };
 
   // EventHandler.once('work.finished', (work) => {
   //   if (work.id === createdWork.id) onFinish(work);
   // })
+
+  workQueue.push(work);
+
+}
+
+
+async function processWorkQueue() {
+
+  const work = workQueue.shift();
+
+  if (!work) {
+    return setTimeout(processWorkQueue, 3000);
+  }
+
+
+  const worker = workers.find(it => it.type === work.type);
+
+  if (!worker) {
+    throw new Error('no worker for this work type ${}');
+  }
+
+
+  work.stage = 'active';
+
+
+  const { stage, type, context } = await worker.handler({
+    work,
+  });
+
+
+  work.stage = stage;
+  work.type = type;
+  work.context = context;
+
+  work.history.push({
+    stage,
+    type,
+    context,
+    createdAt: Date.now(),
+  });
+
 
 }
